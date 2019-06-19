7fb2e4cff95234506d86fc0d1be34bdc18bbb6a6
Emin Durak <emin@pomegra.org>
2019-06-19 15:53:02 +0200

    returnig user

--- server/accounts/accounts-general.js
+++ server/accounts/accounts-general.js
@@ -1,20 +1,20 @@
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
+import { Meteor } from 'meteor/meteor'
+import { Accounts } from 'meteor/accounts-base'
 
 getWelcomeEmailText = username => {
-  return `Hi ${username},\n\nWe are delighted to have you at Skogen.\nBy being a subscriber, you can easily take part in our public events and groups.\n\nRegards,\nSkogen Team`;
-};
+  return `Hi ${username},\n\nWe are delighted to have you at Skogen.\nBy being a subscriber, you can easily take part in our public events and groups.\n\nRegards,\nSkogen Team`
+}
 
 Accounts.onCreateUser((options, user) => {
-  user.attending = [];
-  user.groups = [];
-  user.notifications = [];
+  user.attending = []
+  user.groups = []
+  user.notifications = []
   if (user.services.facebook) {
-    user.username = user.services.facebook.name;
-    user.emails = [{ address: user.services.facebook.email }];
+    user.username = user.services.facebook.name
+    user.emails = [{ address: user.services.facebook.email }]
   } else if (user.services.google) {
-    user.username = user.services.google.name;
-    user.emails = [{ address: user.services.google.email }];
+    user.username = user.services.google.name
+    user.emails = [{ address: user.services.google.email }]
   }
   // Meteor.call(
   //   'sendEmail',
@@ -23,5 +23,5 @@ Accounts.onCreateUser((options, user) => {
   //   getWelcomeEmailText(user.username),
   //   true // is new user
   // );
-  // return user;
-});
+  return user
+})
